0ff4958725a7c347e72f453072603998f560d887
yosh-25 <yoshi.okada25@gmail.com>
2024-02-25 15:41:00 +0900

    login画面の設定中

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 import { SessionProvider, signOut, useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 
 export default function Home() {
   const session = useSession();
-  return (
-    <>
-    
-      <div>{session?.data?.user?.name}</div>
-      <button onClick={() => signOut()}>Logout</button>
-      
-    </>
-  );
+  if (session) {
+    return (
+      <>
+        <div>{session?.data?.user?.name}</div>
+        <button onClick={() => signOut()}>Logout</button>
+      </>
+    );
+  } else {
+    redirect('/')
+  }
 }
